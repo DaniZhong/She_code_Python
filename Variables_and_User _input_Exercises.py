142545e96fcfa0d015ad12a2f3902da7fb51b527
DaniZhong <102441707+DaniZhong@users.noreply.github.com>
2022-04-30 11:44:28 +1000

    Python _lesson_first_week_exercise
    Variables and User Input Exercises

---
+++ Variables_and_User _input_Exercises.py
@@ -0,0 +1,32 @@
+#Variables and User Input: Exercises
+
+#Q1
+num1=input("Please enter a number")
+print(f"Enter a number: {num1}")
+num2=input("Please enter a number")
+print(f"Enter another number:{num2}")
+print(float(num1)+float(num2))
+
+#Q2
+num3=input("Please enter a number")
+print(f"Enter a number: {num3}")
+num4=input("Please enter a number")
+print(f"Enter another number:{num4}")
+Muti=float(num3)*float(num4) 
+print(f"{num3}*{num4}={Muti}")  
+
+#Q3
+dist_km=input("How many kilometres")
+dist_m=float(dist_km)*1000
+dist_cm=float(dist_km)*10000
+print(f"{dist_km}km={dist_m}m")
+print(f"{dist_km}km={dist_cm}cm")
+
+#Q4
+name=input("What is your name?")
+height=input("How tall are you (cms)?")
+print(f"{name} is {height}cms tall.")
+
+
+
+
